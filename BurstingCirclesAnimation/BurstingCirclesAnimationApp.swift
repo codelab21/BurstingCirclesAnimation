95eda2750a2cecb5355ccce4e14a174e86535ea5
codelab21 <codelab21@outlook.com>
2023-07-25 06:20:55 +0300

    Initial Commit

---
+++ BurstingCirclesAnimation/BurstingCirclesAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  BurstingCirclesAnimationApp.swift
+//  BurstingCirclesAnimation
+//
+//  Created by Eymen on 25.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct BurstingCirclesAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
